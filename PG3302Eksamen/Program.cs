ae6036d46cf5cd4ba5e9212fdc69eceb2ccb95c0
dankni95 <daniel_kn@live.no>
2022-10-31 14:12:57 +0100

    Add Prompt handling

--- PG3302Eksamen/Program.cs
+++ PG3302Eksamen/Program.cs
@@ -1,6 +1,8 @@
 ﻿using PG3302Eksamen.Model;
 using PG3302Eksamen.Model.AccountModel;
 using PG3302Eksamen.Repositories;
+using PG3302Eksamen.Utils;
+using Spectre.Console;
 
 namespace PG3302Eksamen;
 
@@ -11,30 +13,39 @@ public static class Program {
         var personRepository = new PersonRepository();
         var transactionRepository = new TransactionRepository();
 
-        var savingAccount = new SavingsAccountFactory().InitializeAccount(12, 5, 10,
-            "Fattig",
-            new Person("Fra ekte repo, runes home", "Rune", "Oliveira", "uno dos",
-                "94474621",
-                "0505040234", new DateTime().Date),
-            "111111111", DateTime.Now);
-        var currentAccount = new CurrentAccountFactory().InitializeAccount(0, 1, 20,
-            "Brukskonto",
-            new Person("Fra ekte repo, runes home", "Daniel", "Lysak", "uno dos tres",
-                "92262913",
-                "06049524733", new DateTime().Date),
-            "222222222", DateTime.Now);
-
-        var bill = new Bill().CreateBill(currentAccount, savingAccount, "With love", 123,
-            BillStatusEnum.PAID,
-            DateTime.Now);
-
-        //billRepository.Insert(bill);
-
-        accountRepository.Insert(savingAccount);
-        accountRepository.Insert(currentAccount);
+        var promptQuestionAnswer = PromptUtil.PromptQuestion("Hello this question comes from a Util/Helper class!");
+        var selectPromptAnswer = PromptUtil.PromptSelectPrompt("Welcome to Jalla Bank, please:", new[] {"Login", "Create Account", "Exit"});
 
 
-        foreach (var account in accountRepository.GetSortedByBalance())
-            Console.WriteLine(account.Balance);
+        if (selectPromptAnswer.Equals("Create Account")) {
+            var savingAccount = new SavingsAccountFactory().InitializeAccount(12, 5, 10,
+                "Fattig",
+                new Person("Fra ekte repo, runes home", "Rune", "Oliveira", "uno dos",
+                    "94474621",
+                    "0505040234", new DateTime().Date),
+                "111111111", DateTime.Now);
+            var currentAccount = new CurrentAccountFactory().InitializeAccount(0, 1, 20,
+                "Brukskonto",
+                new Person("Fra ekte repo, runes home", "Daniel", "Lysak", "uno dos tres",
+                    "92262913",
+                    "06049524733", new DateTime().Date),
+                "222222222", DateTime.Now);
+
+            var bill = new Bill().CreateBill(currentAccount, savingAccount, "With love", 123,
+                BillStatusEnum.PAID,
+                DateTime.Now);
+
+            //billRepository.Insert(bill);
+
+            accountRepository.Insert(savingAccount);
+            accountRepository.Insert(currentAccount);
+
+
+            foreach (var account in accountRepository.GetSortedByBalance())
+                Console.WriteLine(account.Balance);
+            accountRepository.Insert(currentAccount);
+        }
     }
 }
+
+
---
+++ PG3302Eksamen/Utils/PromptUtil.cs
@@ -0,0 +1,27 @@
+using Spectre.Console;
+
+namespace PG3302Eksamen.Utils;
+
+public class PromptUtil {
+    private static string msg;
+
+    public static void setMessage(string message) {
+        msg = message;
+    }
+
+    public static string getMessage() {
+        return msg;
+    }
+
+    public static bool PromptQuestion(string text) {
+        return AnsiConsole.Confirm("[green]" + text + "[/]");
+    }
+
+    public static string PromptSelectPrompt(string? title,string[] questions) {
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title(title)
+                .PageSize(10)
+                .AddChoices(questions));
+    }
+}
