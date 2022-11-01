f696a47579be249e450ed38dd959ced45cdbee76
joachr <chimjoa@hotmail.com>
2022-11-01 11:58:20 +0100

    implemented PayBill(), UpdateBillStatus(), and GetSortedByStatus, but have yet to test these because of enum difficulties in db. changed Transfer() so that it inserts a transaction to the db. struggling with GetRecentTransactions()

--- PG3302Eksamen/Interfaces/ITransactionRepository.cs
+++ PG3302Eksamen/Interfaces/ITransactionRepository.cs
@@ -4,7 +4,7 @@ using A_Team.Core.Model.AccountModel;
 namespace A_Team.Core.Interfaces;
 
 public interface ITransactionRepository : IRepository<Transaction> {
-    IQueryable<Transaction> GetRecentTransactions(int days);
-    void PayBill(Bill bill);
+    List<Transaction> GetRecentTransactions(int days);
+    void PayBill(int billId, string fromAccountNr);
     void Transfer(int accountFromId, int accountToId, decimal amount);
 }
