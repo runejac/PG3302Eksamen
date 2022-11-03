5aa7127525513ca9fcdee04d32bffb7cfc24c2cd
dankni95 <daniel_kn@live.no>
2022-11-03 18:31:24 +0100

    Add tests

--- PG3302Eksamen_Tests/UnitTest1.cs
+++ PG3302Eksamen_Tests/UnitTest1.cs
@@ -1,12 +1,52 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PG3302Eksamen.Model;
+using PG3302Eksamen.Repositories;
+
 namespace PG3302Eksamen_Tests;
 
-public class Tests {
-	[SetUp]
-	public void Setup() {
-	}
+public class UnitTest1 {
+    private bool disposedValue; // To detect redundant calls
 
-	[Test]
-	public void Test1() {
-		Assert.Pass();
-	}
+    private BankContext context;
+
+    
+
+    [Test]
+    public void TestMethod() {
+        Person person = new();
+        var service = new PersonRepository(context);
+        service.Insert(person.CreatePerson("uno", "dos", "tres", "lala", "lala", "122222222", "lol"));
+        context.SaveChanges();
+            
+            
+        Assert.That(context.Persons.Count(), Is.EqualTo(1));
+        Assert.That(context.Persons.Single().Address, Is.EqualTo("uno"));
+        
+        
+    }
+    
+    [SetUp]
+    public void CreateContextForInMemory() {
+        var option = new DbContextOptionsBuilder<BankContext>().UseInMemoryDatabase("test_db").Options;
+
+        context = new BankContext(option);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+    
+
+
+    protected virtual void Dispose(bool disposing) {
+        if (!disposedValue) {
+            if (disposing) {
+            }
+
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose() {
+        Dispose(true);
+    }
 }
